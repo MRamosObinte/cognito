b7afe7dd1832fdec8f914e999d4487a8295f6c63
mramosobinte <mramos@obinte.com>
2019-10-01 12:40:12 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+    production: true,
+
+    region: 'us-east-1',
+
+    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
+    userPoolId: 'us-east-1_uJWd5d5Rh',
+    clientId: '6fh1hkat842mn3grrcp0o03cq2',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-1',
+
+    ddbTableName: 'LoginTrail',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+
+};
+
